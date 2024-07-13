62aa10f07b19083b60da4759dd12ee0fd25652df
babakzy <babaksdz@gmail.com>
2024-07-13 17:26:28 +0330

    add tailwindcss and daisyui

--- app.vue
+++ app.vue
@@ -1,6 +1,14 @@
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+<div class=" bg-green-200 h-5">ss</div>
+<button class="btn btn-primary">Button</button>
   </div>
 </template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
