47c80986250e98192e96a5ec54fc2ef373d18a67
babakzy <babaksdz@gmail.com>
2024-10-11 08:06:51 +0330

    add github register

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,8 @@
 <template>
   <div
     class=" bg-gradient-to-b from-slate-200 to-slate-50 pt-[calc(50vh-300px)] md:pt-[calc(50vh-200px)] min-h-[90vh] ">
-    <LoginForm/>
+    <!-- <LoginForm/> -->
+     <NewLoginForm/>
   </div>
 </template>
 
